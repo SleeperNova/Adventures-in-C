455baf707bbae52298f526cb82f6f3a8507091f3
Francisco Saavedra <saavedra.francisco@outlook.com>
2020-09-29 21:51:07 -0500

    syntax info

--- Part-2-The-Operating-Room/7_count.c
+++ Part-2-The-Operating-Room/7_count.c
@@ -16,9 +16,9 @@ int main()
     printf("ctr is at %d\n", ctr); //2
     ctr = ctr + 1;
     printf("ctr is at %d\n", ctr); //3
-    ctr = ctr + 1;
+    ctr +=  1; // another way to add 1 to ctr
     printf("ctr is at %d\n", ctr); //4
-    ctr = ctr + 1;
+    ctr +=  1;
     printf("ctr is at %d\n", ctr); //5
     return 0;
 }

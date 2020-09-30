b4c851396bc441deebb5b32f0d95f9e149a8d2d0
Francisco Saavedra <saavedra.francisco@outlook.com>
2020-09-29 21:42:25 -0500

    simple count from 1 to 5

---
+++ Part-2-The-Operating-Room/count.c
@@ -0,0 +1,24 @@
+/*
+    Francisco Saavedra
+
+    Counts from 1 to 5
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int ctr = 0;
+    ctr = ctr + 1;
+    
+    printf("ctr is at %d\n", ctr); //1
+    ctr = ctr + 1;
+    printf("ctr is at %d\n", ctr); //2
+    ctr = ctr + 1;
+    printf("ctr is at %d\n", ctr); //3
+    ctr = ctr + 1;
+    printf("ctr is at %d\n", ctr); //4
+    ctr = ctr + 1;
+    printf("ctr is at %d\n", ctr); //5
+    return 0;
+}

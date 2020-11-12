da17f1984beda274d48258a0e786a032725b0fb9
Francisco Saavedra <saavedra.francisco@outlook.com>
2020-11-12 11:13:27 -0600

    simple trig example

---
+++ Part-3-Keeping-Control/20_trig.c
@@ -0,0 +1,21 @@
+/*
+    Francisco Saavedra 
+
+    Lower to upper case converter
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("The trig functions:\n");
+    printf(" The cosine of 1 is %.3f\n", cos(1));
+    printf(" The sine of 1 is %.3f\n", sin(1));
+    printf(" The tangent of 1 is %.3f\n", tan(1));
+    printf("The log functions:\n");
+    printf(" e raised to 1 is %.3f\n", exp(1));
+    printf(" The natural log of 2 is %.3f\n", log(2));
+    printf(" The base 10 log of 2 is %.3f\n", log10(2));
+    return 0;
+}
